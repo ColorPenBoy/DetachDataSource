6ccdeca4b282aaa44a00df18fe8d546aac2cfdf3
colorPen <colorPen@zq.local>
2015-06-11 17:46:14 +0800

    Initial Commit

---
+++ tableView分离/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tableView分离
+//
+//  Created by colorPen on 15/6/11.
+//  Copyright (c) 2015年 colorPen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tableView分离/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tableView分离
+//
+//  Created by colorPen on 15/6/11.
+//  Copyright (c) 2015年 colorPen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
